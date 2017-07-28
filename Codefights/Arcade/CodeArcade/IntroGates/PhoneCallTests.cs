6524ad37037d67c892302a9098716e2f6efe7a0e
nerobianchi <nerobianchi@gmail.com>
2017-07-28 14:59:37 +0300

    initial

---
+++ Codefights/Arcade/CodeArcade/IntroGates/PhoneCallTests.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+
+namespace Codefights.Arcade.CodeArcade.IntroGates
+{
+    public class PhoneCallTests
+    {
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void gwt(int min1, int min2_10, int min11, int s, int expected)
+        {
+            Sut sut = new Sut();
+            var actual = sut.phoneCall(min1, min2_10, min11, s);
+
+            Assert.Equal(expected, actual);
+        }
+
+        private static object[][] TestData()
+        {
+            return new[]
+                   {
+                       new object[]
+                       {
+                           3, 1, 2, 20, 14
+                       },
+                       new object[]
+                       {
+                           2, 2, 1, 2, 1
+                       },
+                       new object[]
+                       {
+                           10, 1, 2, 22, 11
+                       }
+                   };
+        }
+    }
+}
---
+++ Codefights/Arcade/CodeArcade/IntroGates/Sut.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Codefights.Arcade.CodeArcade.IntroGates
+{
+    class Sut
+    {
+        int addTwoDigits(int n)
+        {
+            int total = 0;
+            foreach (var s in n.ToString())
+            {
+                total += Int32.Parse(s.ToString());
+            }
+            return total;
+        }
+        int largestNumber(int n)
+        {
+            return (int)Math.Pow(10, n) - 1;
+        }
+        int candies(int n, int m)
+        {
+            return m - (int)Math.Floor((decimal)(m % n));
+        }
+
+        int maxMultiple(int divisor, int bound)
+        {
+            return bound - (int)Math.Floor((decimal)(bound % divisor));
+
+        }
+        int lateRide(int n)
+        {
+            var hour = Math.Floor((decimal)(n / 60));
+            var min = n % 60;
+
+
+            return (int)(Math.Floor(hour / 10) + (hour % 10) + Math.Floor((decimal)(min / 10)) + (min % 10));
+
+        }
+        public int phoneCall(int min1, int min2_10, int min11, int s)
+        {
+            if (s < min1)
+            {
+                return 0;
+            }
+            int remaining = s - min1;
+
+            var total = 1;
+
+            if (remaining > min2_10 * 9)
+            {
+                total += 9;
+                remaining = remaining - 9 * min2_10;
+            }
+            else
+            {
+                int i = remaining / min2_10;
+                return total + i;
+            }
+
+            int i1 = remaining / min11;
+            total += i1;
+            return total;
+        }
+    }
+}

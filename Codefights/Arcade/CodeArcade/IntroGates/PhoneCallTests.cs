8aad48ba6234d4539223954fcfdcf0c0d24485f0
erdem <nerobianchi@gmail.com>
2017-08-02 21:38:58 +0300

    g

--- Codefights/Arcade/CodeArcade/IntroGates/PhoneCallTests.cs
+++ Codefights/Arcade/CodeArcade/IntroGates/PhoneCallTests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 
 namespace Codefights.Arcade.CodeArcade.IntroGates
 {
@@ -17,20 +18,116 @@ namespace Codefights.Arcade.CodeArcade.IntroGates
         private static object[][] TestData()
         {
             return new[]
-                   {
-                       new object[]
-                       {
-                           3, 1, 2, 20, 14
-                       },
-                       new object[]
-                       {
-                           2, 2, 1, 2, 1
-                       },
-                       new object[]
-                       {
-                           10, 1, 2, 22, 11
-                       }
-                   };
+            {
+                new object[]
+                {
+                    3, 1, 2, 20, 14
+                },
+                new object[]
+                {
+                    2, 2, 1, 2, 1
+                },
+                new object[]
+                {
+                    10, 1, 2, 22, 11
+                }
+            };
         }
+
+        [Theory]
+        [MemberData(nameof(TestData3))]
+        public void gwtt(int a, int b, int c,bool expected)
+        {
+            var actual = arithmeticExpression(a, b, c);
+            
+            Assert.Equal(expected, actual);
+        }
+
+        bool arithmeticExpression(int a, int b, int c)
+        {
+            if (a + b == c) return true;
+            if (a - b == c) return true;
+            if (a * b == c) return true;
+            if ((float)a / b == c ) return true;
+            return false;
+        }
+        
+        private static object[][] TestData3()
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    8,3,2,false
+                },
+            };
+        }
+        
+        
+        
+        
+        [Theory]
+        [MemberData(nameof(TestData2))]
+        public void gwtt(int a, int b,bool expected)
+        {
+            var actual = tennisSet(a, b);
+            
+            Assert.Equal(expected, actual);
+        }
+        private static object[][] TestData2()
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    7,2,false
+                },new object[]
+                {
+                    7,6,true
+                },new object[]
+                {
+                    7,5,true
+                },
+            };
+        }
+        
+        
+//            if(score1 == 5 || score2 == 5)
+//            {
+//                return Math.Abs(score1-score2) == 2 ;
+//            }
+
+//            if(max > 6 || min > 6){
+//                return  Math.Abs(max-min) == 2    ;
+//            }
+//            return Math.Abs(max-min) >= 2;   
+
+        
+        
+        bool tennisSet(int score1, int score2) {
+            
+            int max = score1;
+            int min = score2;
+            if(score1 < score2)
+            {
+                max=score2;
+                min = score1;
+            }
+            
+            if (max==min)
+            {
+                return false;
+            }
+            if (max == 6 && min < 5)
+            {
+                return true;
+            }
+            if (max == 7 && (min == 5 || min == 6))
+                return true;
+            
+            return false;
+
+        }
+
     }
 }
